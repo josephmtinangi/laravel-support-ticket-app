b41ca04614f656e0127826ed4e73f2478b09c7d5
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-16 11:16:59 +0300

    Store a ticket in database

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -14,6 +14,12 @@ use Auth;
 
 class TicketController extends Controller
 {
+
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
     public function create(){
     	$categories = Category::all();
     	return view('tickets.create', compact('categories'));
